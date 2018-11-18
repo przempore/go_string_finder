02dd9add163652ea87b4d288024f4fa94daf65d0
Przemek Porebski <przempore@gmail.com>
2018-11-18 13:26:37 +0100

    trying to split string | WIP

--- string_finder.go
+++ string_finder.go
@@ -3,34 +3,68 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+	"regexp"
+	"strings"
 )
 
 type Reader struct {
-	DataInString []string
+	DataInString    string
+	DataInRawString string
+	DataInLines     []string
 }
 
-func (r Reader) GetFileData(file_path *string) error {
+func (r *Reader) GetFileData(file_path *string) error {
 	data, err := ioutil.ReadFile(*file_path)
 	if err != nil {
 		fmt.Println("Can't read file:", file_path)
 		return err
 	}
 
-	for _, element := range data {
-		r.DataInString = append(r.DataInString, string(element))
+	r.DataInRawString = string(data)
+	reg, err := regexp.Compile("[^a-zA-Z0-9]+")
+	if err != nil {
+		log.Fatal(err)
 	}
+	r.DataInString = reg.ReplaceAllString(string(data), " ")
 
 	return nil
 }
 
-func (r Reader) PrintFileContent() {
-	fmt.Println("File content is:")
-	fmt.Println(r.DataInString)
+func (r *Reader) PrintFileContent() {
+	//fmt.Println("File content is:")
+	//fmt.Println(r.DataInRawString)
 }
 
-func (r Reader) FindString(string_to_find *string) string {
+func (r *Reader) FindString(string_to_find *string) string {
+	tmp := strings.Split(r.DataInString, "\n")
+	fmt.Println(tmp)
 
-	return "empty"
+	fmt.Printf("lines: %d\nWhich is:\n", len(r.DataInLines))
+	for _, l := range r.DataInLines {
+		fmt.Println(l)
+	}
+
+	fmt.Println("the end of lines")
+
+	words := make([][]string, len(r.DataInLines))
+	for ln, l := range r.DataInLines {
+		wordsInLine := strings.Fields(l)
+		words[ln] = make([]string, len(wordsInLine))
+		for wn, w := range words[ln] {
+			words[ln][wn] = w
+		}
+	}
+
+	fmt.Println("-------------------------------- words --------------------------------")
+	fmt.Println(words)
+	fmt.Println("-------------------------------- words --------------------------------")
+
+	for _, w := range words {
+		fmt.Println(w)
+	}
+
+	return "Hello, world"
 }
 
 func Hello() string {
